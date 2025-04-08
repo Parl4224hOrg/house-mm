b76df9996fd93ec21932c17059817e170aef7bed
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2025-04-08 12:16:09 -0500

    fix signup flow

--- src/buttons/signup.ts
+++ src/buttons/signup.ts
@@ -3,6 +3,7 @@ import {ButtonBuilder} from "@discordjs/builders";
 import {ButtonStyle} from "discord.js";
 import {logError} from "../loggers";
 import {register} from "../modals/register";
+import tokens from "../tokens";
 
 export const signup: Button = {
     data: new ButtonBuilder()
@@ -11,7 +12,12 @@ export const signup: Button = {
         .setStyle(ButtonStyle.Success),
     run: async (interaction) => {
         try {
-            await interaction.showModal(register.data);
+            const member = await interaction.guild!.members.fetch(interaction.user);
+            await member.roles.add(tokens.Player);
+            await interaction.reply({
+                ephemeral: true,
+                content: `Go to <#${tokens.RegionSelect}> to select a region (required)\nGo to <#${tokens.SNDReadyChannel}> to ready up or use \`/ready 5v5\`\nTo change your registered id use \`/register\` or the button above`,
+            });
         } catch (e) {
             await logError(e, interaction);
         }
